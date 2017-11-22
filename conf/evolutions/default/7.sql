ee4862a667ea33b220b26b2e8049a0cf7faf4f2e
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-11-22 21:27:04 +0100

    admin insert bug fix

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -1,4 +1,5 @@
 # --- !Ups
+DELETE FROM account WHERE id=1;
 INSERT INTO account (id,email,password,role,phone,country,city,first_name,last_name) VALUES (1,'admin@abh.ba','$2y$10$nDBBMbUbZJa/s1XTzjlV6.3YOwgm/uAwwI12z2KkxH16Q0KKwdpDy','ADMIN','000','Bosnia and Herzegovina','Sarajevo','admin','admin');
 # --- !Downs
 DELETE FROM account WHERE id=1;
