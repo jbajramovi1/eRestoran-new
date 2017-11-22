c08b3a6b2cbfd4bb82215c13179d7fba6d311bc9
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-11-22 22:01:10 +0100

    auto apply downs

---
+++ conf/evolutions/default/8.sql
@@ -0,0 +1,4 @@
+# --- !Ups
+INSERT INTO account (id,email,password,role,phone,country,city,first_name,last_name) VALUES (10,'admin@abh.ba','$2y$10$nDBBMbUbZJa/s1XTzjlV6.3YOwgm/uAwwI12z2KkxH16Q0KKwdpDy','ADMIN','000','Bosnia and Herzegovina','Sarajevo','admin','admin');
+# --- !Downs
+DELETE FROM account WHERE role='ADMIN';
