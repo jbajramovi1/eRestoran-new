5f2063fd40095e38c1283a4c943b75c1f50e3d89
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-12-19 12:31:37 +0100

    RES-2new:Tables component FE

--- public/eRestaurant/app/components/restaurant-tables.js
+++ public/eRestaurant/app/components/restaurant-tables.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tables:[{id:'1',people:'2',amount:'0'}],
+  tables:[{id:'1',people:'2',amount:''}],
   tablesId:1,
   actions:{
     addItem:function(){
         this.set('tablesId',this.get('tablesId')+1);
-        this.get('tables').pushObject({id:this.get('tablesId'),people:'2',amount:'0'});
+        this.get('tables').pushObject({id:this.get('tablesId'),people:'2',amount:''});
     }
   }
 });
